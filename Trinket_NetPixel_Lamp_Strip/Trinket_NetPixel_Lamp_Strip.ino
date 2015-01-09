e4ac9c06d458db454e7073e721cc49e8de4060a6
Tim Batzel <tbatzel@gmail.com>
2015-01-09 00:50:59 -0800

    Added blinking fadeout, copied from floatingskull's pendent rings.

--- Trinket_NetPixel_Lamp_Strip/Trinket_NetPixel_Lamp_Strip.ino
+++ Trinket_NetPixel_Lamp_Strip/Trinket_NetPixel_Lamp_Strip.ino
@@ -7,28 +7,38 @@
 #define PIXEL_COUNT 60
 #define BRIGHTNESS  25      //0-255  
 
-#define MAX_MODES 6
+#define FADE_SPAWN_RATE 10  // How many fade cycles, before drawing something new. Default: 20
+#define FADE_SPEED 5        // How long it takes to fade out. Default: 5
 
-int SWPwrBl = 4;
-int SWModeGr = 8;
 
-int SWPwrBlLED = 5;
-int SWModeGrLED = 6; 
+#define MAX_MODES 7
 
-int mode = 0;
+uint8_t SWPwrBl = 4;
+uint8_t SWModeGr = 8;
 
+uint8_t SWPwrBlLED = 5;
+uint8_t SWModeGrLED = 6; 
+
+uint8_t mode = 0;
+
+// Define colors
                     //    Red, Grn, Blu, Ylw, Wht
 uint8_t userRed[6] = { 0, 255,   0,   0, 200, 255 };
 uint8_t userGrn[6] = { 0,   0, 255,   0, 150, 255 };
 uint8_t userBlu[6] = { 0,   0,   0, 255,   0, 255 };
 
+
+uint8_t valRed[PIXEL_COUNT];
+uint8_t valGrn[PIXEL_COUNT];
+uint8_t valBlu[PIXEL_COUNT];
+
+
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
-
-
-
 
 void setup() {
 
+  randomSeed(analogRead(0));
+  
   pinMode(SWPwrBlLED, OUTPUT);
   pinMode(SWModeGrLED, OUTPUT);
 
@@ -82,6 +92,9 @@ void loop() {
       case 5:
           PixelRandom();
           break;
+      case 6:
+          PixelRandomFade();
+          break;
       default:
          PixelsSolidSet(50,50,50);
     }
@@ -104,13 +117,52 @@ void loop() {
 // Functions
 
 void PixelRandom() {
-    int num = random(PIXEL_COUNT);
-    int col = random(5) + 1;
+    uint8_t num = random(PIXEL_COUNT);
+    uint8_t col = random(5) + 1;
     
     strip.setPixelColor(num, userRed[col], userGrn[col], userBlu[col]);
     strip.show();
     delay(10);
 }
+
+void PixelRandomFade(){
+    uint8_t num = random(PIXEL_COUNT);
+    uint8_t col = random(5) + 1;
+  
+    valRed[num] = userRed[col];
+    valGrn[num] = userGrn[col];
+    valBlu[num] = userBlu[col];
+ 
+   FadeDimPixels(FADE_SPAWN_RATE);
+}
+
+
+void FadeDraw(){
+    uint32_t tempColor;
+    for(uint8_t i = 0; i < PIXEL_COUNT; i++) {
+      tempColor = strip.Color(valRed[i], valGrn[i], valBlu[i]);
+      strip.setPixelColor(i, tempColor);
+    }
+    strip.show();  
+}
+
+
+// dims pixels, but doesn't account for colors like yellow, which fade to red(200), once green(150) runs out.
+void FadeDimPixels(uint8_t count) {
+  for(uint8_t num = 0; num < count; num++) {
+    for(uint8_t i = 0; i < PIXEL_COUNT; i++) {
+      if(valRed[i] > 0) { valRed[i]--; }
+      if(valGrn[i] > 0) { valGrn[i]--; }
+      if(valBlu[i] > 0) { valBlu[i]--; }
+    } 
+    FadeDraw();
+    delay(FADE_SPEED);
+  }
+}
+
+
+
+
 
 void PixelsSolidSet(uint8_t r, uint8_t g, uint8_t b){
     // Flash Black
